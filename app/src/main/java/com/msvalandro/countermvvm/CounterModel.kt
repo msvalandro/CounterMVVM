937e7cdb1b4f91ac82a51e3d6aeaad2087ccc142
Matheus Valandro <msvalandro@proton.me>
2024-10-01 20:01:08 +0100

    refactor: implement counter model class

--- app/src/main/java/com/msvalandro/countermvvm/CounterModel.kt
+++ app/src/main/java/com/msvalandro/countermvvm/CounterModel.kt
@@ -1,3 +1,17 @@
 package com.msvalandro.countermvvm
 
 data class CounterModel(var count: Int)
+
+class CounterRepository {
+    private var _counter = CounterModel(0)
+
+    fun getCounter() = _counter
+
+    fun incrementCounter() {
+        _counter.count++
+    }
+
+    fun decrementCounter() {
+        _counter.count--
+    }
+}
