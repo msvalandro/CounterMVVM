cd5193da7290d6251ca3fed4aaf402055d66e8ae
Matheus Valandro <msvalandro@proton.me>
2024-10-01 19:53:35 +0100

    initial commit with project files

---
+++ app/src/main/java/com/msvalandro/countermvvm/CounterModel.kt
@@ -0,0 +1,3 @@
+package com.msvalandro.countermvvm
+
+data class CounterModel(var count: Int)
---
+++ app/src/main/java/com/msvalandro/countermvvm/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.msvalandro.countermvvm
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.msvalandro.countermvvm.ui.theme.CounterMVVMTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val viewModel: CounterViewModel by viewModels()
+
+            CounterMVVMTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    CounterApp(viewModel)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CounterApp(viewModel: CounterViewModel) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Count: ${viewModel.count.value}", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row {
+            Button(onClick = { viewModel.increment() }) {
+                Text("Increment")
+            }
+            Button(onClick = { viewModel.decrement() }) {
+                Text("Decrement")
+            }
+        }
+    }
+}
